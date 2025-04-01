f4cf01f661bea8193576de3137e9c93b61827c4e
LuizGarbini <garbasneto@gmail.com>
2025-04-01 14:55:27 -0300

    Middleware Global

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,28 @@
-import express from "express"
+import express from "express";
+import { myMiddleware } from "./middlewares/my-middleware";
 
-const PORT = 3333
+const PORT = 3333;
 
-const app = express()
+const app = express();
+
+// Utilizando o middleware de forma global.
+app.use(myMiddleware);
 
 // Definimos que o padrão de dados recebidos é JSON
-app.use(express.json())
+app.use(express.json());
 
 app.get("/products", (request, response) => {
-  // Extraindo os query params de um jeito mais fácil
-  const { page, limit } = request.query
+	// Extraindo os query params de um jeito mais fácil
+	const { page, limit } = request.query;
 
-  response.send(`Página ${page} de ${limit}`)
-})
+	response.send(`Página ${page} de ${limit}`);
+});
 
 app.post("/products", (request, response) => {
-  const { name, price} = request.body
+	const { name, price } = request.body;
 
-  // response.send(`Produto ${name} custa $ ${price}`)
-  response.status(201).json({name, price})
-})
+	// response.send(`Produto ${name} custa $ ${price}`)
+	response.status(201).json({ name, price });
+});
 
-app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
