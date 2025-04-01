809db8f8791d98e5cc51793271f559083b10cfb5
LuizGarbini <garbasneto@gmail.com>
2025-04-01 15:18:09 -0300

    Middleware Local

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ const PORT = 3333;
 
 const app = express();
 
-// Utilizando o middleware de forma global.
-app.use(myMiddleware);
+// Middleware global (aplica para todas as rotas abaixo.)
+// app.use(myMiddleware);
 
 // Definimos que o padrão de dados recebidos é JSON
 app.use(express.json());
@@ -18,7 +18,7 @@ app.get("/products", (request, response) => {
 	response.send(`Página ${page} de ${limit}`);
 });
 
-app.post("/products", (request, response) => {
+app.post("/products", myMiddleware, (request, response) => {
 	const { name, price } = request.body;
 
 	// response.send(`Produto ${name} custa $ ${price}`)
