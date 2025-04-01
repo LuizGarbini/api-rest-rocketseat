36f788cc0f393ff1cfb337e3ac7a8f1d1907de98
LuizGarbini <garbasneto@gmail.com>
2025-04-01 13:57:47 -0300

    Dados no corpo da requisição

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,20 @@ const PORT = 3333
 
 const app = express()
 
+// Definimos que o padrão de dados recebidos é JSON
+app.use(express.json())
+
 app.get("/products", (request, response) => {
   // Extraindo os query params de um jeito mais fácil
   const { page, limit } = request.query
 
   response.send(`Página ${page} de ${limit}`)
+})
+
+app.post("/products", (request, response) => {
+  const { name, price} = request.body
+
+  response.send(`Produto ${name} custa $ ${price}`)
 })
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
