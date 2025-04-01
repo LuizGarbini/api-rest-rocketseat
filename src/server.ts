3762b51b390ca5ca90d454f3382c42227babb522
LuizGarbini <garbasneto@gmail.com>
2025-04-01 11:01:07 -0300

    Query Params

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,11 @@ const PORT = 3333
 
 const app = express()
 
-app.get("/products/:id/:user", (request, response) => {
-  const { id, user } = request.params
-  response.send(`Produto ${id} do usuário ${user}`)
+app.get("/products", (request, response) => {
+  // Extraindo os query params de um jeito mais fácil
+  const { page, limit } = request.query
+
+  response.send(`Página ${page} de ${limit}`)
 })
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
