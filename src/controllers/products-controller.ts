eb60e30657b472f9953cfebfd1700c96d029c8c3
LuizGarbini <garbasneto@gmail.com>
2025-04-02 11:58:57 -0300

    Finalizando o projeto

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -30,26 +30,6 @@ class ProductsController {
 
 		const { name, price } = bodySchema.parse(request.body);
 
-		/*
-		if (!name) {
-			throw new AppError("Nome do produto é obrigatório!");
-		}
-
-		if (name.trim().length < 6) {
-			throw new AppError(
-				"Nome do produto precisa ter pelo menos 6 caracteres!",
-			);
-		}
-
-		if (!price) {
-			throw new AppError("Preço do produto é obrigatório!");
-		}
-
-		if (price < 0) {
-			throw new AppError("Preço do produto não pode ser menor que zero!");
-		}
-			*/
-
 		//throw new AppError("Erro ao tentar criar um produto!");
 
 		// response.send(`Produto ${name} custa $ ${price}`)
