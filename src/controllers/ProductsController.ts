dd08eec52c02173647414204ee4e0fd8b2f5f523
LuizGarbini <garbasneto@gmail.com>
2025-04-01 22:09:04 -0300

    AppError

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -19,6 +19,8 @@ class ProductsController {
 	create(request: Request, response: Response) {
 		const { name, price } = request.body;
 
+		//throw new AppError("Erro ao tentar criar um produto!");
+
 		// response.send(`Produto ${name} custa $ ${price}`)
 		response.status(201).json({ name, price, user_id: request.user_id });
 	}
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express, {
 	type Response,
 } from "express";
 import { routes } from "./routes";
+import { AppError } from "./utils/AppError";
 
 const PORT = 3333;
 
@@ -14,9 +15,17 @@ app.use(express.json());
 
 app.use(routes);
 
+/**
+ * 400 (Bad Request): Error do cliente.
+ * 500 (Internal Server Error): Erro interno do servidor.
+ */
+
 app.use(
 	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 	(error: any, request: Request, response: Response, _: NextFunction) => {
+		if (error instanceof AppError) {
+			return response.status(error.statusCode).json({ message: error.message });
+		}
 		response.status(500).json({ message: error.message });
 	},
 );
